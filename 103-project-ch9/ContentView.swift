ab72a8316cb3a9ca4264c86dfe38f89a9d8110a5
Carlos Vera <vera.0214@outlook.com>
2026-05-16 14:16:34 -0500

    Initial Commit

---
+++ 103-project-ch9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103-project-ch9
+//
+//  Created by Carlos Vera on 5/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103-project-ch9/_03_project_ch9App.swift
@@ -0,0 +1,17 @@
+//
+//  _03_project_ch9App.swift
+//  103-project-ch9
+//
+//  Created by Carlos Vera on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _03_project_ch9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
