efad63f8d9895334ac4c937176891d4ad47262cf
Carlos Vera <vera.0214@outlook.com>
2026-05-23 16:09:21 -0500

    Final Assignment

---
+++ 103-project-ch9/Assignments/ProfessorListView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfessorListView.swift
+//  103-project-ch9
+//
+//  Created by Carlos Vera on 5/23/26.
+//
+
+import SwiftUI
+
+struct ProfessorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfessorListView()
+}
